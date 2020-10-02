fc2d672686b90559184ec6ef5fb4845e4726bc9b
Aylin GÜNEŞ <47246248+aylin1080p@users.noreply.github.com>
2020-10-03 02:23:29 +0300

    Initial commit

---
+++ app/src/main/java/com/aylingunes/layouttry/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aylingunes.layouttry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
